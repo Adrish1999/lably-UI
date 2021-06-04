2b6c67472841690ff2491778eeaff172f6658d2c
Adrish Bose <adrishbose198@gmail.com>
2021-06-04 23:50:53 +0530

    Components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AdminsComponent } from './admins/admins.component';
+import { LabsComponent } from './labs/labs.component';
+import { TestsComponent } from './tests/tests.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home' , component: HomeComponent},
+  {path: 'admins' , component: AdminsComponent},
+  {path: 'labs' , component: LabsComponent},
+  {path: 'tests' , component: TestsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
